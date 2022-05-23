08d73734eb511c5f17ce8a53943001e982f2adf2
P.Konstantin <kostya.potekaev@gmail.com>
2022-05-23 18:04:43 +0300

    Сделал 4 номер

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -64,8 +64,19 @@ namespace Lesson4
         }
         static void task4()
         {
-
+            Console.Write("Введите значение:");
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(f(n));
         }
+        static long f(long n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1 || n == 2)
+                return 1;
+            else
+                return (f(n - 1) + f(n - 2));
+;        }
 
     }
 }
